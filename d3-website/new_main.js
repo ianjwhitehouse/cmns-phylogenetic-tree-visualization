6f1fa55e6317a29faa4012be58e796586f011d1a
bte_lead <ian.whitehouse88@gmail.com>
2025-04-29 19:53:23 -0400

    Added bars to show frequencies

--- d3-website/new_main.js
+++ d3-website/new_main.js
@@ -35,27 +35,54 @@ d3.json('tree.json').then(function(dataset) {
     var nodeEnter = nodeG.selectAll('.node')
         .data(dataset)
         .enter()
-        .append('rect')
-        .attr('class', 'node')
-        .attr('r', 12)
-        .attr('y', function (d) {
-            return (height/max_level) * (d.level) - 35;
-        })
-        .attr("x", function (d) {
-            return d.x - 75;
-        })
-        .attr("width", 150).attr("height", 70).on("click", function (d) {
-            const clickedElement = d3.select(this)
+        .append('g')
+        .attr('transform', function (d) {
+            return 'translate(' + d.x + ',' + (height/max_level) * d.level + ')';
+        }).on("click", function (d) {
             this.parentNode.appendChild(this);
+            const rect = d3.select(this).select('rect');
+            const title = d3.select(this).select('text');
 
-            if (clickedElement.attr("width") < 200) {
-                make_big(clickedElement, d, max_level);
+            if (rect.attr("width") < 200) {
+                d3.select(this).select('.g-pre-expand').attr("opacity", 0.0);
+                d3.select(this).select('.g-post-expand').transition().attr("opacity", 1.0);
+                make_big(rect, title, d, max_level);
             }
             else {
-                make_small(clickedElement, d, max_level);
+                d3.select(this).select('.g-pre-expand').transition().attr("opacity", 1.0);
+                d3.select(this).select('.g-post-expand').attr("opacity", 0.0);
+                make_small(rect, title, d, max_level);
             }
-
         });
+
+    nodeEnter.append('rect')
+        .attr('class', 'node')
+        .attr('y', function (d) {
+            return -35;
+        })
+        .attr("x", function (d) {
+            return -90;
+        })
+        .attr("width", 180).attr("height", 70)
+        .attr("fill", "white")
+
+    nodeEnter.append('text').attr('font-size','16px') .attr('font-weight','bold').attr('text-anchor', 'middle')
+        .attr("alignment-baseline", "text-before-edge").attr("dy", -35).attr('class', 'node-title').text(function (d) {
+        return (
+            "Cluster " + d.cluster + " (" + d.mutations + " Mutations)"
+        );
+    });
+
+    var pre_expand = nodeEnter.append('g').attr("class", "g-pre-expand")
+    var post_expand = nodeEnter.append('g').attr("class", "g-post-expand").attr("opacity", 0.0)
+
+    var prob_bars = pre_expand.append('g').attr('transform', 'translate(-60, 35)').attr('fill', 'blue').attr('stroke', 'black');
+    prob_bars.append('rect').attr('class', 'bar').attr("x", 0).attr("width", 30).attr('height', d => 35 * d.avg_obs_freq[0]).attr('y', d => -35 * d.avg_obs_freq[0])
+    prob_bars.append('rect').attr('class', 'bar').attr("x", 30).attr("width", 30).attr('height', d => 35 * d.avg_obs_freq[1]).attr('y', d => -35 * d.avg_obs_freq[1])
+    prob_bars.append('rect').attr('class', 'bar').attr("x", 60).attr("width", 30).attr('height', d => 35 * d.avg_obs_freq[2]).attr('y', d => -35 * d.avg_obs_freq[2])
+    prob_bars.append('rect').attr('class', 'bar').attr("x", 90).attr("width", 30).attr('height', d => 35 * d.avg_obs_freq[3]).attr('y', d => -35 * d.avg_obs_freq[3])
+    prob_bars.append('line').attr("x1", 0).attr("x2", 120).attr("y1", -35).attr("y2", -35).attr('stroke-width', 2).attr('stroke', 'red');
+
 
     var linkEnter = linkG.selectAll('.link')
         .data(dataset.filter(x => x["level"] > 0))
@@ -85,36 +112,32 @@ d3.json('tree.json').then(function(dataset) {
         })
 })
 
-function make_big(element, d, max_level) {
-    if ((height/max_level) * (d.level) - 105 < 0) {
-        y = 0;
+function make_big(rect, title, d, max_level) {
+    x = -225
+    if (d.x + x < 0) {
+        x = 0 - d.x
     }
-    else if ((height/max_level) * (d.level) + 105 > height) {
-        y = height - 210;
+    else if (d.x - x > width) {
+        x = d.x - width
     }
-    else {
-        y = (height/max_level) * (d.level) - 105
+    y = -105
+    if (((height/max_level) * d.level) + y < 0) {
+        y = 0 - ((height/max_level) * d.level)
     }
-
-    if (d.x - 225 < 0) {
-        x = 0;
+    else if (((height/max_level) * d.level) - y > height) {
+        y = ((height/max_level) * d.level) - height
     }
-    else if (d.x + 225 > width) {
-        x = width - 450;
-    }
-    else {
-        x = d.x - 225;
-    }
-
-    element.transition().attr('y', y).attr("x", x).attr("width", 450).attr("height", 210)
+    rect.transition().attr('y', y).attr("x", x).attr("width", 450).attr("height", 210)
+    title.transition().attr('dy', y).attr('font-size', '32px')
 }
 
-function make_small(element, d, max_level) {
-    element.transition().attr('y', function (d) {
-        return (height/max_level) * (d.level) - 35;
+function make_small(rect, title, d, max_level) {
+    rect.transition().attr('y', function (d) {
+        return -35;
     })
     .attr("x", function (d) {
-        return d.x - 75;
+        return -90;
     })
-    .attr("width", 150).attr("height", 70)
+    .attr("width", 180).attr("height", 70)
+    title.transition().attr('dy', -35).attr('font-size', '16px')
 }
